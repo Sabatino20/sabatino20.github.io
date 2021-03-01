74fd40f9a444f2b9c858032e4508f34dab4224b6
Sabatino <dino.russo2000@hotmail.com>
2021-03-01 11:56:01 +0100

    Sito V.1

---
+++ menu.js
@@ -0,0 +1,15 @@
+  // Disabilita lo scroll quando il menu responsive è aperto
+    $('#test').change(function() {
+    if ($('#test').is(":checked")) {
+    $('body').css('overflow','hidden');
+    } else {
+    $('body').css('overflow','auto');
+    }
+});
+
+$(document).ready(function(){
+  $('.chiudi').click(function(){
+    $( "#test" ).prop( "checked", false );
+    $('body').css('overflow','auto'); 
+  }); 
+});
