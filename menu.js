5254fb27ce0a8891e327e89b4d251630b7756452
Sabatino <dino.russo2000@hotmail.com>
2021-11-14 17:53:30 +0100

    Sito V.2

--- menu.js
+++ menu.js
@@ -1,15 +1,15 @@
-  // Disabilita lo scroll quando il menu responsive è aperto
-    $('#test').change(function() {
-    if ($('#test').is(":checked")) {
-    $('body').css('overflow','hidden');
-    } else {
-    $('body').css('overflow','auto');
-    }
+// Disabilita lo scroll quando il menu responsive è aperto
+$("#test").change(function () {
+  if ($("#test").is(":checked")) {
+    $("body").css("overflow", "hidden");
+  } else {
+    $("body").css("overflow", "auto");
+  }
 });
 
-$(document).ready(function(){
-  $('.chiudi').click(function(){
-    $( "#test" ).prop( "checked", false );
-    $('body').css('overflow','auto'); 
-  }); 
+$(document).ready(function () {
+  $(".chiudi").click(function () {
+    $("#test").prop("checked", false);
+    $("body").css("overflow", "auto");
+  });
 });
